c2c573f20c574bec58d0c49ba5584ac98be3f89f
Andrew Rowe <roweandrewjohn2@gmail.com>
2024-02-10 04:22:35 -0500

    Add search index to player

--- server/crud-read.js
+++ server/crud-read.js
@@ -4,18 +4,41 @@ const uri = "mongodb+srv://bigshaq:FGdvZXvULQ3kGY3B@nbashotdata.sqfjle1.mongodb.
 const client = new MongoClient(uri);
 async function run() {
   try {
+    console.time("executionTime"); // Start measuring execution time
+
     await client.connect();
     // database and collection
     const db = client.db("shot_data");
     const coll = db.collection("main");
 
-    // use .find() to retrieve all docs
-    const cursor = coll.find();
-    // iterate code goes here
-    await cursor.forEach(console.log);
+    // Create an index on player
+
+    // Define the search index pipeline
+    const pipeline = [
+      {
+        $search: {
+          index: "default",
+          text: {
+            query: "Lebron James",
+            path: {
+              wildcard: "*"
+            }
+          }
+        }
+      }
+    ];
+
+    // Execute the search index pipeline
+    const cursor = await coll.aggregate(pipeline);
+
+    await cursor.forEach(doc => {
+      console.log(`shotX: ${doc.shotX}, shotY: ${doc.shotY}, made: ${doc.made}`);
+  });
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
+    console.timeEnd("executionTime"); // End measuring execution time
+
   }
 }
 run().catch(console.dir);
