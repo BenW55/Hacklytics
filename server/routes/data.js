b095a99da00a712287ddd4ec0ae049c94ff2df1f
Shaurya Sehgal <reachshauryasehgal@gmail.com>
2024-02-10 04:15:34 -0500

    court

--- server/routes/data.js
+++ server/routes/data.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const fs = require('fs').promises; // Note: Using promises version for async/await
+const path = require('path');
 const router = express.Router();
 
-router.get('/', (req,res) => {
-  res.json({message: "test"});
+router.get('/', async (req,res) => {
+  try {
+    const dataDir = path.join(__dirname, 'json_data.json'); // Correctly defining the directory path
+    
+    const file = await fs.readFile(dataDir, 'utf-8'); // Correctly using 'await' within async function
+    
+    
+    
+    res.json({ data : file });
+  } catch (error) {
+    console.error('Error reading data directory:', error);
+    res.status(500).send('Server error');
+  }
 });
 
 module.exports = router;
