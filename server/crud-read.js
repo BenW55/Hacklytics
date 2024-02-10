dd5136ac40236c53132d6349f308a376836107ab
Andrew Rowe <roweandrewjohn2@gmail.com>
2024-02-10 02:17:42 -0500

    Add basic mongodb calls

---
+++ server/crud-read.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require("mongodb");
+// connection string.
+const uri = "mongodb+srv://purplepeopleeater:QfSvBpAF9lYX86e9@cluster0.y2zjw5l.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    await client.connect();
+    // database and collection
+    const db = client.db("sample_guides");
+    const coll = db.collection("planets");
+
+    // use .find() to retrieve all docs
+    const cursor = coll.find({ hasRings: false, mainAtomsphere: "Ar" });
+    // iterate code goes here
+    await cursor.forEach(console.log);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
